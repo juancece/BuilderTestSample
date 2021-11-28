1cc4a9bb81052dafd5f9a4371c8a9f654bee7e4d
Juan.carmona <juancece@outlook.com>
2021-11-27 20:44:27 -0500

    Inital version

---
+++ src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
@@ -0,0 +1,23 @@
+using BuilderTestSample.Exceptions;
+using BuilderTestSample.Services;
+using BuilderTestSample.Tests.TestBuilders;
+using Xunit;
+
+namespace BuilderTestSample.Tests
+{
+    public class OrderServicePlaceOrder
+    {
+        private readonly OrderService _orderService = new ();
+        private readonly OrderBuilder _orderBuilder = new ();
+
+        [Fact]
+        public void ThrowsExceptionGivenOrderWithExistingId()
+        {
+            var order = _orderBuilder
+                            .WithId(123)
+                            .Build();
+
+            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+        }
+    }
+}
